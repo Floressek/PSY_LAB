a1ccd344eabc4ee63bf9f67d79cedc448e91b83b
Floressek <szymon.florek@student.wat.edu.pl>
2024-11-17 01:20:44 +0100

    Innit for lab2

---
+++ src/lab_2/Stanowisko.java
@@ -0,0 +1,32 @@
+package lab_2;
+
+public class Stanowisko {
+
+    // Parametry stanowiska
+    private boolean zajete;
+    private double czasKoncaObslugi;
+    private Interesant aktywnyInteresant; // Interesant, ktory jest obslugiwany
+
+    public Stanowisko() {
+        this.zajete = false;
+        this.czasKoncaObslugi = 0.0;
+    }
+
+    // Getters
+    public boolean isZajete() {
+        return zajete;
+    }
+
+    // Rozpoczecie obslugi interesanta
+    public void rozpocznijObsluge(Interesant interesant, double czasObslugi) {
+        this.zajete = true;
+        this.aktywnyInteresant = interesant;
+        this.czasKoncaObslugi = czasObslugi;
+    }
+
+    // Zakonczenie obslugi interesanta
+    public void zakonczObsluge() {
+        this.zajete = false;
+        this.aktywnyInteresant = null;
+    }
+}
